ab78a92c703bbcb52b7e61f526c6def550eede37
Zester Quinn <zester.a@freshclinics.com>
2023-07-30 00:28:11 +0800

    update metadata export

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
     description: 'Gender Reveal Party Invitation',
     title: 'Gender Reveal',
     images: [
-      'https://mail.google.com/mail/u/0?ui=2&ik=3dcf481910&attid=0.1&permmsgid=msg-f:1772762710385379611&th=189a1e4ae794491b&view=fimg&realattid=f_lko2d27j6&disp=thd&attbid=ANGjdJ9finesMqrTenPGLe66uSFgbYNnyhmndgJvKcw7zekde8ET_It1uXAjJsbjdY2HbeXBUyT7K5GL1tLl7xh7fEfLJXyb4sBhdcQtpNY_uDEUAC7fIfCpvr5nvhI&ats=2524608000000&sz=w1920-h938'
-    ]
+     'https://instagram.fmnl18-1.fna.fbcdn.net/v/t51.2885-15/363835210_2856735644456780_7084995320807450544_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fmnl18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=W89PMPYog8UAX_ds7Mk&edm=ACWDqb8BAAAA&ccb=7-5&_nc_e2o=f&oh=00_AfDWXX3dJCSTJeRqObXRfFi0IRe4RpBH_bWY8z8G2T_dLg&oe=64C95278&_nc_sid=ee9879'
+      ]
   }
 }
 
